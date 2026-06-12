d36785140a32afc84dc63c3aa25bea054e3df6fb
Tony Sousa <167309815+tonysousa19@users.noreply.github.com>
2026-06-12 10:45:54 -0300

    Update system_info.sh

--- system_info.sh
+++ system_info.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
 
+echo "====== SYSTEM INFO ======"
+
+echo "User:"
+whoami
+
+echo ""
+
+echo "Hostname:"
+hostname
+
+echo ""
+
+echo "Kernel:"
+uname -a
+
+echo ""
+
+echo "Disk:"
+df -h
+
+echo ""
+
+echo "Memory:"
+free -h
